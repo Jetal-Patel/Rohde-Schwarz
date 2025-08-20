ecb245f91a5023b861beaa364e5227ab3e680bd4
Jetal-Patel <jpatel275901@gmail.com>
2025-08-19 21:52:29 -0700

    SMW to FSW Power Level Fine Tuning.py

--- SMW to FSW Power Level Fine Tuning.py
+++ SMW to FSW Power Level Fine Tuning.py
@@ -5,6 +5,8 @@ and increment upwards until the desired power is achieved on the FSW.
 from RsInstrument import *
 import time
 
+from fontTools.misc.cython import returns
+
 # Initialize the session
 smw = RsInstrument('TCPIP::192.168.8.66::hislip0', reset=False)
 
@@ -19,9 +21,11 @@ idn = fsw.query('*IDN?')
 print(f"\nHello, I am: '{idn}'")
 
 
-TARGET_FSW_POWER = -10.00 # Set the desired power at the FSW
-SMW_STARTING_POWER = -30 # Set the starting output power of the SMW
+TARGET_FSW_POWER = -1 # Set the desired power at the FSW
+SMW_STARTING_POWER = -5 # Set the starting output power of the SMW
+MAX_SMW_POWER = 0 # Set the max power the SMW will put out
 CW_FREQUENCY = 1000000000 # Set the frequency of the CW signal
+REF_LEVEL = TARGET_FSW_POWER + 10
 
 smw.write_with_opc(f'SOUR1:POW:LEV:IMM:AMPL {SMW_STARTING_POWER}')
 smw.write_with_opc('OUTP1:STAT 1')
@@ -31,6 +35,7 @@ fsw.write_with_opc(':INIT:CONT OFF')
 fsw.write_with_opc(f':SENS:FREQ:CENT {CW_FREQUENCY}')
 fsw.write_with_opc(':SENS:FREQ:SPAN 200000000')
 fsw.write_with_opc('SENS:BAND:RES 1000000')
+fsw.write_with_opc(f':DISP:WIND:TRAC:Y:SCAL:RLEV {REF_LEVEL}')
 fsw.write_with_opc(':CALC1:MARK1:STAT ON')
 fsw.write_with_opc(':CALC1:MARK1:MAX:PEAK')
 fsw.write_with_opc(':INIT:IMM')
@@ -42,9 +47,14 @@ while abs(FSW_POWER - TARGET_FSW_POWER) >= 0.50:
     fsw.write_with_opc(':INIT:IMM')
     fsw.write_with_opc(':CALC1:MARK1:MAX:PEAK')
     FSW_POWER = float(fsw.query(':CALC1:MARK1:Y?'))
-    SMW_OUT_PWR += 0.25
-    smw.write(f'SOUR1:POW:LEV:IMM:AMPL {SMW_OUT_PWR}')
-    time.sleep(0.5)
+    if SMW_OUT_PWR < MAX_SMW_POWER:
+        SMW_OUT_PWR += 0.25
+        smw.write(f'SOUR1:POW:LEV:IMM:AMPL {SMW_OUT_PWR}')
+        time.sleep(0.5)
+    else:
+        print("SMW Power has reached user defined limit")
+        break
+
 else: print("FSW Power is now at the desired level!")
 
 smw.close()
